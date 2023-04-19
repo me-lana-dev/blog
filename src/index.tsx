be56176fdad4f151c987473de4034e92e3128302
Lana Kaliush <me.lana.dev@gmail.com>
2023-04-19 15:21:46 +0300

    fix routes

--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename="/blog">
       <App />
     </BrowserRouter>
   </Provider>
